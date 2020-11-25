7800dd1940cd75ffb1e28750cd4a43b5aaab2ea4
Harrison Cook <hcook@raygun.io>
2020-11-26 10:28:27 +1300

    Renames Demo from AwesomeTSProject to Raygun4ReactNativeDemo

--- demo/ios/Raygun4ReactNativeDemoTests/Raygun4ReactNativeDemo.m
+++ demo/ios/Raygun4ReactNativeDemoTests/Raygun4ReactNativeDemo.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeTSProjectTests : XCTestCase
+@interface Raygun4ReactNativeDemoTests : XCTestCase
 
 @end
 
-@implementation AwesomeTSProjectTests
+@implementation Raygun4ReactNativeDemoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
