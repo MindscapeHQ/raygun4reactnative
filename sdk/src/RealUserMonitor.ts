6d5158957c63133951a0ecc3b3eb8f72597e70c6
Harrison Cook <hcook@raygun.io>
2020-12-07 11:35:05 +1300

    Concatenates sendRUMPayload into sendRUMEvent

--- sdk/src/RealUserMonitor.ts
+++ sdk/src/RealUserMonitor.ts
@@ -12,130 +12,122 @@ const SessionRotateThreshold = 30 * 60 * 100;
 
 export default class RealUserMonitor {
 
-    private getCurrentUser: () => User;
-    private apiKey: string;
-    private version: string;
-    private disableNetworkMonitoring: boolean;
-    private customRealUserMonitoringEndpoint: string;
-    private ignoredURLs: string[];
-    private RAYGUN_RUM_ENDPOINT = 'https://api.raygun.com/events';
+  private getCurrentUser: () => User;
+  private apiKey: string;
+  private version: string;
+  private disableNetworkMonitoring: boolean;
+  private customRealUserMonitoringEndpoint: string;
+  private ignoredURLs: string[];
+  private RAYGUN_RUM_ENDPOINT = 'https://api.raygun.com/events';
 
-    lastActiveAt = Date.now();
-    curRUMSessionId: string = '';
+  lastActiveAt = Date.now();
+  curRUMSessionId: string = '';
 
-    constructor(getCurrentUser: () => User, apiKey: string, disableNetworkMonitoring = true, ignoredURLs: string[], customRealUserMonitoringEndpoint: string, version: string) {
+  constructor(getCurrentUser: () => User, apiKey: string, disableNetworkMonitoring = true, ignoredURLs: string[], customRealUserMonitoringEndpoint: string, version: string) {
 
-        // Assign the values parsed in (assuming initiation is the only time these are altered).
-        this.apiKey = apiKey;
-        this.disableNetworkMonitoring = disableNetworkMonitoring;
-        this.ignoredURLs = ignoredURLs;
-        this.customRealUserMonitoringEndpoint = customRealUserMonitoringEndpoint;
-        this.getCurrentUser = getCurrentUser;
-        this.version = version;
+    // Assign the values parsed in (assuming initiation is the only time these are altered).
+    this.apiKey = apiKey;
+    this.disableNetworkMonitoring = disableNetworkMonitoring;
+    this.ignoredURLs = ignoredURLs;
+    this.customRealUserMonitoringEndpoint = customRealUserMonitoringEndpoint;
+    this.getCurrentUser = getCurrentUser;
+    this.version = version;
 
-        if (!disableNetworkMonitoring) {
-            setupNetworkMonitoring(
-                ignoredURLs.concat(defaultURLIgnoreList, customRealUserMonitoringEndpoint || []),
-                this.generateNetworkTimingEventCallbackMethod.bind(this)
-            );
-        }
+    if (!disableNetworkMonitoring) {
+      setupNetworkMonitoring(
+        ignoredURLs.concat(defaultURLIgnoreList, customRealUserMonitoringEndpoint || []),
+        this.generateNetworkTimingEventCallbackMethod.bind(this)
+      );
+    }
 
-        this.lastActiveAt = Date.now();
-        this.curRUMSessionId = '';
+    this.lastActiveAt = Date.now();
+    this.curRUMSessionId = '';
 
-        const eventEmitter = new NativeEventEmitter(RaygunNativeBridge);
-        eventEmitter.addListener(RaygunNativeBridge.ON_START, this.reportStartupTime.bind(this));
-        eventEmitter.addListener(RaygunNativeBridge.ON_PAUSE, this.markLastActiveTime.bind(this));
-        eventEmitter.addListener(RaygunNativeBridge.ON_RESUME, this.rotateRUMSession.bind(this));
-        eventEmitter.addListener(RaygunNativeBridge.ON_DESTROY, () => {
-            eventEmitter.removeAllListeners(RaygunNativeBridge.ON_START);
-            eventEmitter.removeAllListeners(RaygunNativeBridge.ON_PAUSE);
-            eventEmitter.removeAllListeners(RaygunNativeBridge.ON_RESUME);
-            eventEmitter.removeAllListeners(RaygunNativeBridge.ON_DESTROY);
-        });
+    const eventEmitter = new NativeEventEmitter(RaygunNativeBridge);
+    eventEmitter.addListener(RaygunNativeBridge.ON_START, this.reportStartupTime.bind(this));
+    eventEmitter.addListener(RaygunNativeBridge.ON_PAUSE, this.markLastActiveTime.bind(this));
+    eventEmitter.addListener(RaygunNativeBridge.ON_RESUME, this.rotateRUMSession.bind(this));
+    eventEmitter.addListener(RaygunNativeBridge.ON_DESTROY, () => {
+      eventEmitter.removeAllListeners(RaygunNativeBridge.ON_START);
+      eventEmitter.removeAllListeners(RaygunNativeBridge.ON_PAUSE);
+      eventEmitter.removeAllListeners(RaygunNativeBridge.ON_RESUME);
+      eventEmitter.removeAllListeners(RaygunNativeBridge.ON_DESTROY);
+    });
 
+  };
+
+
+  generateNetworkTimingEventCallbackMethod(name: string, sendTime: number, duration: number) {
+    const data = {name, timing: {type: RealUserMonitoringEvents.NetworkCall, duration}};
+    this.sendRUMEvent(RealUserMonitoringEvents.EventTiming, data, sendTime).catch();
+  };
+
+
+  markLastActiveTime = async () => {
+    this.lastActiveAt = Date.now();
+  };
+
+  async rotateRUMSession(payload: Record<string, any>) {
+    if (Date.now() - this.lastActiveAt > SessionRotateThreshold) {
+      this.lastActiveAt = Date.now();
+      await this.sendRUMEvent(RealUserMonitoringEvents.SessionEnd, {});
+      this.curRUMSessionId = getDeviceBasedId();
+      return this.sendRUMEvent(RealUserMonitoringEvents.SessionStart, {});
+    }
+  };
+
+
+  async sendRUMEvent(eventName: string, data: Record<string, any>, timeAt?: number) {
+    const timestamp = timeAt ? new Date(timeAt) : new Date();
+    const rumMessage = {
+      type: eventName,
+      timestamp: timestamp.toISOString(),
+      user: this.getCurrentUser(),
+      sessionId: this.curRUMSessionId,
+      version: this.version,
+      os: Platform.OS,
+      osVersion,
+      platform,
+      data: JSON.stringify([data])
     };
 
-
-    generateNetworkTimingEventCallbackMethod(name: string, sendTime: number, duration: number) {
-        const data = {name, timing: {type: RealUserMonitoringEvents.NetworkCall, duration}};
-        this.sendRUMEvent(RealUserMonitoringEvents.EventTiming, data, sendTime).catch();
-    };
-
-
-    markLastActiveTime = async () => {
-        this.lastActiveAt = Date.now();
-    };
-
-    async rotateRUMSession(payload: Record<string, any>) {
-        if (Date.now() - this.lastActiveAt > SessionRotateThreshold) {
-            this.lastActiveAt = Date.now();
-            await this.sendRUMEvent(RealUserMonitoringEvents.SessionEnd, {});
-            this.curRUMSessionId = getDeviceBasedId();
-            return this.sendRUMEvent(RealUserMonitoringEvents.SessionStart, {});
-        }
-    };
+    return fetch(this.customRealUserMonitoringEndpoint || this.RAYGUN_RUM_ENDPOINT, {
+      method: 'POST',
+      headers: {'X-ApiKey': this.apiKey, 'Content-Type': 'application/json'},
+      body: JSON.stringify({eventData: [rumMessage]})
+    }).catch(err => {
+      log(err);
+    });
+  };
 
 
-    async sendRUMEvent(eventName: string, data: Record<string, any>, timeAt?: number) {
-        const timestamp = timeAt ? new Date(timeAt) : new Date();
-        const rumMessage = {
-            type: eventName,
-            timestamp: timestamp.toISOString(),
-            user: this.getCurrentUser(),
-            sessionId: this.curRUMSessionId,
-            version: this.version,
-            os: Platform.OS,
-            osVersion,
-            platform,
-            data: JSON.stringify([data])
-        };
-
-        return this.sendRUMPayload(rumMessage, this.apiKey, this.customRealUserMonitoringEndpoint);
-    };
-
-
-    sendCustomRUMEvent(
-        getCurrentUser: () => User,
-        apiKey: string,
-        eventType: RealUserMonitoringEvents.ActivityLoaded | RealUserMonitoringEvents.NetworkCall,
-        name: string,
-        duration: number,
-        customRealUserMonitoringEndpoint?: string
-    ) {
-        if (eventType === RealUserMonitoringEvents.ActivityLoaded) {
-            this.reportStartupTime(name, duration);
-            return;
-        }
-        if (eventType === RealUserMonitoringEvents.NetworkCall) {
-            this.generateNetworkTimingEventCallbackMethod(name, Date.now() - duration, duration);
-            return;
-        }
-        warn('Unknown RUM event type:', eventType);
-    };
+  sendCustomRUMEvent(
+    getCurrentUser: () => User,
+    apiKey: string,
+    eventType: RealUserMonitoringEvents.ActivityLoaded | RealUserMonitoringEvents.NetworkCall,
+    name: string,
+    duration: number,
+    customRealUserMonitoringEndpoint?: string
+  ) {
+    if (eventType === RealUserMonitoringEvents.ActivityLoaded) {
+      this.reportStartupTime(name, duration);
+      return;
+    }
+    if (eventType === RealUserMonitoringEvents.NetworkCall) {
+      this.generateNetworkTimingEventCallbackMethod(name, Date.now() - duration, duration);
+      return;
+    }
+    warn('Unknown RUM event type:', eventType);
+  };
 
 
-    async reportStartupTime(name: string, duration: number) {
-        if (!this.curRUMSessionId) {
-            this.curRUMSessionId = getDeviceBasedId();
-            await this.sendRUMEvent(RealUserMonitoringEvents.SessionStart, {});
-        }
-        const data = {name, timing: {type: RealUserMonitoringEvents.ActivityLoaded, duration}};
-        return this.sendRUMEvent(RealUserMonitoringEvents.EventTiming, data);
-    };
+  async reportStartupTime(name: string, duration: number) {
+    if (!this.curRUMSessionId) {
+      this.curRUMSessionId = getDeviceBasedId();
+      await this.sendRUMEvent(RealUserMonitoringEvents.SessionStart, {});
+    }
+    const data = {name, timing: {type: RealUserMonitoringEvents.ActivityLoaded, duration}};
+    return this.sendRUMEvent(RealUserMonitoringEvents.EventTiming, data);
+  };
 
-
-//-------------------------------------------------------------------------------------------------
-// SENDING RUM PAYLOADS
-//-------------------------------------------------------------------------------------------------
-
-    async sendRUMPayload(event: Record<string, any>, apiKey: string, customRealUserMonitoringEndpoint?: string) {
-        return fetch(customRealUserMonitoringEndpoint || this.RAYGUN_RUM_ENDPOINT, {
-            method: 'POST',
-            headers: {'X-ApiKey': apiKey, 'Content-Type': 'application/json'},
-            body: JSON.stringify({eventData: [event]})
-        }).catch(err => {
-            log(err);
-        });
-    };
 }
