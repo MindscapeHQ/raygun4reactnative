82c9144c1de08c46a92f69ba8a51c580596a257e
Harrison Cook <hcook@raygun.io>
2020-12-08 12:02:47 +1300

    Adds comments for RealUserMonitor.ts and Utils.ts

--- sdk/src/RealUserMonitor.ts
+++ sdk/src/RealUserMonitor.ts
@@ -10,6 +10,9 @@ const defaultURLIgnoreList = ['api.raygun.com', 'localhost:8081/symbolicate'];
 const SessionRotateThreshold = 30 * 60 * 100;
 
 
+/**
+ * The Real User Monitor class is responsible for managing all logic for RUM specific tasks.
+ */
 export default class RealUserMonitor {
 
   private readonly currentSession: Session;
@@ -22,6 +25,15 @@ export default class RealUserMonitor {
   lastActiveAt = Date.now();
   curRUMSessionId: string = '';
 
+  /**
+   * RealUserMonitor: Manages RUM specific logic tasks.
+   * @param currentSession - The session shared between the CrashReporter and RaygunClient.
+   * @param apiKey - The User's API key that gives them access to RUM. (User provided)
+   * @param disableNetworkMonitoring - If true, XHRInterceptor is not switched on. All requests go through without monitoring.
+   * @param ignoredURLs - A string array of URLs to ignore when watching the network.
+   * @param customRealUserMonitoringEndpoint - The custom API URL endpoint where this API should send data to.
+   * @param version - The Version number of this application. (User provided)
+   */
   constructor(currentSession: Session, apiKey: string, disableNetworkMonitoring = true, ignoredURLs: string[], customRealUserMonitoringEndpoint: string, version: string) {
 
     // Assign the values parsed in (assuming initiation is the only time these are altered).
@@ -31,6 +43,8 @@ export default class RealUserMonitor {
     this.currentSession = currentSession;
     this.version = version;
 
+    // If the USER has not defined disabling network monitoring, setup the XHRInterceptor (see
+    // NetworkMonitor.ts).
     if (!disableNetworkMonitoring) {
       setupNetworkMonitoring(
         ignoredURLs.concat(defaultURLIgnoreList, customRealUserMonitoringEndpoint || []),
@@ -41,6 +55,7 @@ export default class RealUserMonitor {
     this.lastActiveAt = Date.now();
     this.curRUMSessionId = '';
 
+    // Create native event listeners on this device
     const eventEmitter = new NativeEventEmitter(RaygunNativeBridge);
     eventEmitter.addListener(RaygunNativeBridge.ON_START, this.reportStartupTime.bind(this));
     eventEmitter.addListener(RaygunNativeBridge.ON_PAUSE, this.markLastActiveTime.bind(this));
@@ -55,23 +70,42 @@ export default class RealUserMonitor {
   };
 
 
+  /**
+   * Sends a RUMEvent with the parameters parsed into this method. Utilizing the JSON layout sent
+   * to api.raygun.com, the name and duration are added as parameters to the "DATA" field in the
+   * RUM message.
+   * @param name - The event name (note this is not the event type), used in the "DATA" param of a
+   * RUM message
+   * @param sendTime - The time at which the event occurred.
+   * @param duration - The time taken for this event to fully execute.
+   */
   sendNetworkTimingEventCallback(name: string, sendTime: number, duration: number) {
     const data = {name, timing: {type: RealUserMonitoringEvents.NetworkCall, duration}};
     this.sendRUMEvent(RealUserMonitoringEvents.EventTiming, data, sendTime).catch();
   };
 
 
+  /**
+   * Updates the time since last activity to be NOW.
+   */
   markLastActiveTime = async () => {
     this.lastActiveAt = Date.now();
   };
 
+  /**
+   * Enables the ability to send a custom RUM message. Utilizing the parameters described below,
+   * each one is used in constructing a RUM message, which is ultimately fed to the sendRUMEvent
+   * method.
+   * @param eventType - A small description of the event (used to categorize events)
+   * @param name - The name of the event (makes the event individual from it's category)
+   * @param duration - How long this event took to execute.
+   */
   sendCustomRUMEvent(
-    apiKey: string,
     eventType: RealUserMonitoringEvents.ActivityLoaded | RealUserMonitoringEvents.NetworkCall,
     name: string,
-    duration: number,
-    customRealUserMonitoringEndpoint?: string
+    duration: number
   ) {
+
     if (eventType === RealUserMonitoringEvents.ActivityLoaded) {
       this.reportStartupTime(name, duration);
       return;
@@ -84,7 +118,14 @@ export default class RealUserMonitor {
   };
 
 
-  async rotateRUMSession(payload: Record<string, any>) {
+  /**
+   * "Rotating" a RUM session is to close down the current session and open another. Instances where
+   * a rotation is needed:
+   *  anon_user -> user = NO (login)
+   *  user1 -> user2 = YES (switch accounts)
+   *  user -> anon = YES (logout)
+   */
+  async rotateRUMSession() {
     if (Date.now() - this.lastActiveAt > SessionRotateThreshold) {
       this.lastActiveAt = Date.now();
       await this.sendRUMEvent(RealUserMonitoringEvents.SessionEnd, {});
@@ -94,6 +135,13 @@ export default class RealUserMonitor {
   };
 
 
+  /**
+   * Sends a POST request to the custom || default RUM Endpoint, creating an object (later
+   * JSON.stringify-ing this object) with the eventName, data, and time recorded in the message.
+   * @param eventName - A custom name for the "TYPE" of RUM message
+   * @param data - Extra information to send in the RUM message, under "DATA".
+   * @param timeAt - The time at which this event occurred, defaults to NOW if undefined/null.
+   */
   async sendRUMEvent(eventName: string, data: Record<string, any>, timeAt?: number) {
     const timestamp = timeAt ? new Date(timeAt) : new Date();
     const rumMessage = {
@@ -118,6 +166,13 @@ export default class RealUserMonitor {
   };
 
 
+  /**
+   * This method sends a mobile event timing message to the raygun server. If the current session
+   * has not been setup, this method will also ensure that the session has been allocated an ID
+   * before sending away any data.
+   * @param name - Name of the event (specific to the event).
+   * @param duration - How long the event took.
+   */
   async reportStartupTime(name: string, duration: number) {
     if (!this.curRUMSessionId) {
       this.curRUMSessionId = getDeviceBasedId();
--- sdk/src/Utils.ts
+++ sdk/src/Utils.ts
@@ -1,17 +1,25 @@
-import { NativeModules } from 'react-native';
-import { StackFrame } from 'react-native/Libraries/Core/Devtools/parseErrorStack';
-const { RaygunNativeBridge } = NativeModules;
+import {NativeModules} from 'react-native';
+import {StackFrame} from 'react-native/Libraries/Core/Devtools/parseErrorStack';
+
+const {RaygunNativeBridge} = NativeModules;
+
+const SOURCE_MAP_PREFIX = 'file://reactnative.local/';
+const devicePathPattern = /^(.*@)?.*\/[^\.]+(\.app|CodePush)\/?(.*)/;
+const internalTrace = new RegExp('ReactNativeRenderer-dev\\.js$|MessageQueue\\.js$|native\\scode');
 
 //-------------------------------------------------------------------------------------------------
 // GENERAL
 //-------------------------------------------------------------------------------------------------
 
+/**
+ * Constructs an ID specific for the current device being used.
+ */
 export const getDeviceBasedId = () =>
   `${RaygunNativeBridge.DEVICE_ID}-${Date.now().toString(32)}-${(Math.random() * 100000).toString(16).replace('.', '')}`;
 
 /**
- * Deep clone an object
- * @param object
+ * Makes a deep clone of some object.
+ * @param object - Object to clone.
  */
 export const clone = <T>(object: T): T => JSON.parse(JSON.stringify(object));
 
@@ -19,66 +27,81 @@ export const clone = <T>(object: T): T => JSON.parse(JSON.stringify(object));
 // REGEX REFACTORING
 //-------------------------------------------------------------------------------------------------
 
-const SOURCE_MAP_PREFIX = 'file://reactnative.local/';
-
-const devicePathPattern = /^(.*@)?.*\/[^\.]+(\.app|CodePush)\/?(.*)/;
-const internalTrace = new RegExp('ReactNativeRenderer-dev\\.js$|MessageQueue\\.js$|native\\scode');
-
+/**
+ * This method cleans the file paths of the errors logged in a stack trace to be localized to the
+ * project. Removing all device specific strings from each path.
+ * @param frames - Stack Trace of some error.
+ */
 export const cleanFilePath = (frames: StackFrame[]): StackFrame[] =>
-    frames.map(frame => {
-        const result = devicePathPattern.exec(frame.file);
-        if (result) {
-            const [_, __, ___, fileName] = result;
-            return { ...frame, file: SOURCE_MAP_PREFIX + fileName };
-        }
-        return frame;
-    });
+  frames.map(frame => {
+    const result = devicePathPattern.exec(frame.file);
+    if (result) {
+      const [_, __, ___, fileName] = result;
+      return {...frame, file: SOURCE_MAP_PREFIX + fileName};
+    }
+    return frame;
+  });
 
 /**
- * Ensure a given report data payload uses uppercase keys
- * @param obj A report data payload or an array of report data payloads
+ * Ensure a given report data payload uses uppercase keys.
+ * @param obj - A report data payload or an array of report data payloads
  */
 export const upperFirst = (obj: any | any[]): any | any[] => {
-    if (Array.isArray(obj)) {
-        return obj.map(upperFirst);
-    }
-    if (typeof obj === 'object') {
-        return Object.entries(obj).reduce(
-            (all, [key, val]) => ({
-                ...all,
-                ...(key !== 'customData'
-                    ? { [key.slice(0, 1).toUpperCase() + key.slice(1)]: upperFirst(val) }
-                    : { CustomData: val })
-            }),
-            {}
-        );
-    }
-    return obj;
+  if (Array.isArray(obj)) {
+    return obj.map(upperFirst);
+  }
+  if (typeof obj === 'object') {
+    return Object.entries(obj).reduce(
+      (all, [key, val]) => ({
+        ...all,
+        ...(key !== 'customData'
+          ? {[key.slice(0, 1).toUpperCase() + key.slice(1)]: upperFirst(val)}
+          : {CustomData: val})
+      }),
+      {}
+    );
+  }
+  return obj;
 };
 
 /**
  * Remove the '(address at' suffix added by stacktrace-parser which used by React
  * @param frame StackFrame
  */
-export const noAddressAt = ({ methodName, ...rest }: StackFrame): StackFrame => {
-    const pos = methodName.indexOf('(address at');
-    return {
-        ...rest,
-        methodName: pos > -1 ? methodName.slice(0, pos).trim() : methodName
-    };
+export const noAddressAt = ({methodName, ...rest}: StackFrame): StackFrame => {
+  const pos = methodName.indexOf('(address at');
+  return {
+    ...rest,
+    methodName: pos > -1 ? methodName.slice(0, pos).trim() : methodName
+  };
 };
 
+/**
+ * Removes the HTTP || HTTPS protocols from some URL (string).
+ * @param url - The URL to remove the protocol from.
+ */
 export const removeProtocol = (url: string) => url.replace(/^http(s)?:\/\//i, '');
 
 //-------------------------------------------------------------------------------------------------
 // FILTERING
 //-------------------------------------------------------------------------------------------------
 
+/**
+ * Used as a boolean filtering method, returns true if the 'url' exists in the list of urls
+ * to ignore ('ignoredURLs')
+ * @param url - Current URL to determine.
+ * @param ignoredURLs - All URLs that should be ignored.
+ */
 export const shouldIgnore = (url: string, ignoredURLs: string[]): boolean => {
-    const target = removeProtocol(url);
-    return ignoredURLs.some(ignored => target.startsWith(ignored));
+  const target = removeProtocol(url);
+  return ignoredURLs.some(ignored => target.startsWith(ignored));
 };
 
+/**
+ * Used as a boolean filtering method, returns true if the stack frame is above the react native
+ * stack level.
+ * @param frame - The frame in question.
+ */
 export const filterOutReactFrames = (frame: StackFrame): boolean => !!frame.file && !frame.file.match(internalTrace);
 
 //-------------------------------------------------------------------------------------------------
@@ -86,10 +109,10 @@ export const filterOutReactFrames = (frame: StackFrame): boolean => !!frame.file
 //-------------------------------------------------------------------------------------------------
 
 const getLogger = (output: (...args: any[]) => void) => (...args: any[]) => {
-    if (__DEV__) {
-        output(args);
-    }
-    return;
+  if (__DEV__) {
+    output(args);
+  }
+  return;
 };
 
 export const log = getLogger(console.log);
