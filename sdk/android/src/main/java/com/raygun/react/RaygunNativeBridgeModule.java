209edafee6fceeb18b7e2e9f5a1e13cf107c4477
RaygunDaniel <dgibbs@raygun.io>
2021-01-22 14:18:57 +1300

    Android bridge fix

--- sdk/android/src/main/java/com/raygun/react/RaygunNativeBridgeModule.java
+++ sdk/android/src/main/java/com/raygun/react/RaygunNativeBridgeModule.java
@@ -373,3 +373,6 @@ public class RaygunNativeBridgeModule extends ReactContextBaseJavaModule impleme
   }
   //#endregion--------------------------------------------------------------------------------------
 
+}
+
+
