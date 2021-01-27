3545d3e06073a0585d576a9c9dcdfedc032fd888
Harrison Cook <hcook@raygun.io>
2021-01-27 14:58:07 +1300

    Updates Home page to ONLY have global variables

--- demo/screens/Home.tsx
+++ demo/screens/Home.tsx
@@ -1,13 +1,32 @@
-import {Button, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View} from "react-native";
+import {Button, SafeAreaView, ScrollView, StatusBar, Text, View} from "react-native";
 import React, {useState} from "react";
 import {raygunClient} from "../App";
 import {styles} from "../utils/Utils";
-import {Breadcrumb, User} from "raygun4reactnative";
+import {User} from "raygun4reactnative";
+
+
+//#region -- User Objects --------------------------------------------------------------------------
+
+const user1: User = {
+  identifier: "Test_User_1",
+  isAnonymous: false,
+  firstName: "Ronald",
+  fullName: "Ronald Raygun",
+}
+
+const user2: User = {
+  identifier: "Test_User_2",
+  isAnonymous: false,
+  firstName: "Ruby",
+  fullName: "Ruby Raygun",
+}
+
+//#endregion ---------------------------------------------------------------------------------------
 
 export default function Home() {
-  const [tagsBtn, setTagsBtn] = useState("Add Tags");
+
+  const [tagsBtn, setTagsBtn] = useState("Set Tags #1");
   const [userBtn, setUserBtn] = useState("Set User #1");
-  const [breadcrumbsBtn, setBreadcrumbsBtn] = useState("Add Breadcrumb #1");
 
   /**
    * Example of: getTags()
@@ -17,11 +36,11 @@ export default function Home() {
     const clientTags = raygunClient.getTags();
 
     if (clientTags.length > 0) {
-      return clientTags.map((tag) => {
+      return clientTags.map((tag: string) => {
         return (<Text key={`tag: ${tag}`} style={styles.text}>{tag}</Text>)
       })
     } else {
-      return <Text style={styles.text} key={"NOTAGS"}>No Tags current are associated</Text>
+      return <Text key={"NOTAGS"} style={styles.text}>No Tags currently available</Text>
     }
   }
 
@@ -39,47 +58,23 @@ export default function Home() {
     ]
   }
 
-  /**
-   * Example of: getUser()
-   */
-  const showBreadcrumbs = () => {
-    // Get the current user
-    const clientBreadcrumbs: Breadcrumb[] = raygunClient.getBreadcrumbs();
-
-    if (clientBreadcrumbs.length > 0) {
-      console.log("HERE")
-      return (clientBreadcrumbs.map((breadcrumb) => {
-          return ([
-              <Text key={`message${breadcrumb.timestamp}`}>Message: {breadcrumb.message}</Text>,
-              <Text key={`category${breadcrumb.timestamp}`}>Category: {breadcrumb.category}</Text>,
-              <Text key={`level${breadcrumb.timestamp}`}>Level: {breadcrumb.level}</Text>,
-              <Text key={`time${breadcrumb.timestamp}`}>Time: {breadcrumb.timestamp}</Text>,
-              <Text style={styles.secondView}/>]
-          )
-        })
-      )
-    } else {
-      return <Text key={"NOBC"}>No Breadcrumbs current exist</Text>
-    }
-  }
-
 
   /**
    * Example of: setTags(...)
    */
   const setTags = () => {
     switch (tagsBtn) {
-      case "Add Tags":
+      case "Set Tags #1":
         raygunClient.setTags("iPhone 12", "iPhone 11");
-        setTagsBtn("Add Different Tags");
+        setTagsBtn("Set Tags #2");
         return;
-      case "Add Different Tags":
+      case "Set Tags #2":
         raygunClient.setTags("Samsung S21", "Google Pixel 4a");
         setTagsBtn("Remove All Tags");
         return;
       default:
         raygunClient.setTags();
-        setTagsBtn("Add Tags")
+        setTagsBtn("Set Tags #1")
     }
   }
 
@@ -89,68 +84,18 @@ export default function Home() {
   const setUser = () => {
     switch (userBtn) {
       case "Set User #1":
-        const user1: User = {
-          identifier: "Test_User_1",
-          isAnonymous: false,
-          firstName: "Ronald",
-          fullName: "Ronald Raygun",
-        }
         raygunClient.setUser(user1);
         setUserBtn("Set User #2")
         return;
 
       case "Set User #2":
-        const user2: User = {
-          identifier: "Test_User_2",
-          isAnonymous: false,
-          firstName: "Rachel",
-          fullName: "Rachel Raygun",
-        }
         raygunClient.setUser(user2);
         setUserBtn("Set User Anon")
         return;
 
       case "Set User Anon":
-        const anon: User = {
-          identifier: "",
-          isAnonymous: true,
-        }
-        raygunClient.setUser(anon);
+        raygunClient.setUser(null);
         setUserBtn("Set User #1")
-        return;
-    }
-  }
-
-  /**
-   * Example of: addBreadcrumbs
-   */
-  const recordBreadcrumb = () => {
-    switch (breadcrumbsBtn) {
-      case "Add Breadcrumb #1":
-        const bc1: Breadcrumb = {
-          category: "Test",
-          customData: undefined,
-          level: "info",
-          message: "Stage 1",
-          timestamp: Date.now()
-        }
-        raygunClient.recordBreadcrumb(bc1);
-        setBreadcrumbsBtn("Add Breadcrumb #2")
-        return;
-      case "Add Breadcrumb #2":
-        const bc2: Breadcrumb = {
-          category: "Test",
-          customData: undefined,
-          level: "info",
-          message: "Stage 2",
-          timestamp: Date.now()
-        }
-        raygunClient.recordBreadcrumb(bc2);
-        setBreadcrumbsBtn("Clear Breadcrumbs")
-        return;
-      case "Clear Breadcrumbs":
-        raygunClient.clearBreadcrumbs();
-        setBreadcrumbsBtn("Add Breadcrumb #1")
         return;
     }
   }
@@ -164,7 +109,7 @@ export default function Home() {
 
           <View key={"TAGS"} style={styles.mainView}>
             <View style={styles.secondView}>
-              <Text style={styles.title}>Tags:</Text>
+              <Text key={"Tags:"} style={styles.title}>Tags:</Text>
               {showTags()}
             </View>
 
@@ -179,7 +124,7 @@ export default function Home() {
 
           <View key={"USER"} style={styles.mainView}>
             <View style={styles.secondView}>
-              <Text style={styles.title}>User:</Text>
+              <Text key={"User:"} style={styles.title}>User:</Text>
               {showUser()}
             </View>
 
@@ -191,22 +136,6 @@ export default function Home() {
               />
             </View>
           </View>
-
-          <View key={"BREADCRUMBS"} style={styles.mainView}>
-            <View style={styles.secondView}>
-              <Text style={styles.title}>Breadcrumbs:</Text>
-              {showBreadcrumbs()}
-            </View>
-
-            <View style={styles.secondView}>
-              <Button
-                title={breadcrumbsBtn}
-                color={"blue"}
-                onPress={() => recordBreadcrumb()}
-              />
-            </View>
-          </View>
-
         </ScrollView>
       </SafeAreaView>
     </>
