e6c556a1344437573d48a01233920c47a540dad0
RaygunDaniel <dgibbs@raygun.io>
2021-02-03 14:52:34 +1300

    Removes react-native-vector-icons dependency that causes issues with xcode

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Home from "./screens/Home";
 import CrashReporting from "./screens/CrashReporting";
 import RealUserMonitoring from "./screens/RealUserMonitoring";
@@ -20,30 +19,21 @@ function Tabs() {
         name="Home"
         component={Home}
         options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="home" color={color} size={size}/>
-          ),
+          tabBarLabel: 'Home'
         }}
       />
       <Tab.Screen
         name="CrashReporting"
         component={CrashReporting}
         options={{
-          tabBarLabel: 'Crash',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="flash-alert" color={color} size={size}/>
-          ),
+          tabBarLabel: 'Crash'
         }}
       />
       <Tab.Screen
         name="RealUserMonitoring"
         component={RealUserMonitoring}
         options={{
-          tabBarLabel: 'RUM',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="motion-sensor" color={color} size={size}/>
-          ),
+          tabBarLabel: 'RUM'
         }}
       />
     </Tab.Navigator>
