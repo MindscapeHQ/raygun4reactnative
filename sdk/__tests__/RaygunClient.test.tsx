532a3ea841dc4b49bff018593b447210748be7f8
Miguel Beltran <m@beltran.work>
2024-11-18 09:53:41 +0100

    WIP test setup

--- sdk/__tests__/RaygunClient.test.tsx
+++ sdk/__tests__/RaygunClient.test.tsx
@@ -1,13 +1,16 @@
 import { init, sendError } from '../src/RaygunClient';
+import {
+    NativeModules,
+} from 'react-native';
 
 // jest.mock('../RaygunClient');
 
 describe('RaygunClient', () => {
     // let raygunClient: RaygunClient;
 
-    // beforeEach(() => {
-    //     raygunClient = new RaygunClient();
-    // });
+    beforeEach(() => {
+        NativeModules.RaygunNativeBridge = { DEVICE_ID: 'test-device-id' }; 
+    });
 
     it('should send error correctly', () => {
         const error = new Error('Test error');
