0fcf1496262a2fc0896b1659fa97e9f7e57d19fb
Miguel Beltran <m@beltran.work>
2024-11-18 09:58:30 +0100

    mocked native bridge

--- sdk/__tests__/RaygunClient.test.tsx
+++ sdk/__tests__/RaygunClient.test.tsx
@@ -1,16 +1,13 @@
 import { init, sendError } from '../src/RaygunClient';
-import {
-    NativeModules,
-} from 'react-native';
 
 // jest.mock('../RaygunClient');
 
 describe('RaygunClient', () => {
     // let raygunClient: RaygunClient;
 
-    beforeEach(() => {
-        NativeModules.RaygunNativeBridge = { DEVICE_ID: 'test-device-id' }; 
-    });
+    // beforeEach(() => {
+    //     NativeModules.RaygunNativeBridge = { DEVICE_ID: 'test-device-id' }; 
+    // });
 
     it('should send error correctly', () => {
         const error = new Error('Test error');
--- sdk/jest.config.js
+++ sdk/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { rootMode: 'upward' }],
   },
   transformIgnorePatterns: [ 'node_modules/(?!(react-native|@react-native|react-navigation|@react-navigation|@react-native-community|@react-native-firebase|@react-navigation/stack|@react-navigation/bottom-tabs|@react-navigation/drawer|@react-navigation/native|@react-navigation/material-bottom-tabs|@react-navigation/material-top-tabs|@react-navigation/stack|@react-navigation/web))' ],
+  setupFiles: ['./__mocks__/RaygunNativeBridge.js']
 };
