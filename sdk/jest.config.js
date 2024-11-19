a6543a825732c4e126b19373c767e02dd0933eaa
Miguel Beltran <m@beltran.work>
2024-11-19 10:49:55 +0100

    setup working directory

--- sdk/jest.config.js
+++ sdk/jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   preset: 'react-native',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { rootMode: 'upward' }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { rootMode: 'upward' }]
   },
-  transformIgnorePatterns: [ 'node_modules/(?!(react-native|@react-native|react-navigation|@react-navigation|@react-native-community|@react-native-firebase|@react-navigation/stack|@react-navigation/bottom-tabs|@react-navigation/drawer|@react-navigation/native|@react-navigation/material-bottom-tabs|@react-navigation/material-top-tabs|@react-navigation/stack|@react-navigation/web))' ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-navigation|@react-navigation|@react-native-community|@react-native-firebase|@react-navigation/stack|@react-navigation/bottom-tabs|@react-navigation/drawer|@react-navigation/native|@react-navigation/material-bottom-tabs|@react-navigation/material-top-tabs|@react-navigation/stack|@react-navigation/web))'
+  ],
   setupFiles: ['./__mocks__/RaygunNativeBridge.js']
 };
