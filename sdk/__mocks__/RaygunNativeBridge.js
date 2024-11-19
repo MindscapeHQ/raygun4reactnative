0502e8d247370453e1c6d17e0e5ff19fb47974a9
Miguel Beltran <m@beltran.work>
2024-11-19 10:45:37 +0100

    prettier

--- sdk/__mocks__/RaygunNativeBridge.js
+++ sdk/__mocks__/RaygunNativeBridge.js
@@ -1,5 +1,5 @@
-import {NativeModules} from 'react-native';
+import { NativeModules } from 'react-native';
 
 NativeModules.RaygunNativeBridge = {
-  DEVICE_ID: '1234567890',
+  DEVICE_ID: '1234567890'
 };
