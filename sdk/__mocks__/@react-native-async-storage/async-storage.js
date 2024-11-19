27fc4d06bc7a7da399a1699a242112bb774f55ee
Miguel Beltran <m@beltran.work>
2024-11-19 10:59:01 +0100

    fix prettier

--- sdk/__mocks__/@react-native-async-storage/async-storage.js
+++ sdk/__mocks__/@react-native-async-storage/async-storage.js
@@ -3,16 +3,15 @@ export * from '@react-native-async-storage/async-storage/jest/async-storage-mock
 var storage = {};
 
 export default {
-    getItem: (item, value = null) => {
-        return new Promise((resolve, reject) => {
-            storage[item] ? resolve(storage[item]) :
-                resolve(value);
-        });
-    },
-    setItem: (item, value) => {
-        return new Promise((resolve, reject) => {
-            storage[item] = value;
-            resolve(value);
-        });
-    }
-}
+  getItem: (item, value = null) => {
+    return new Promise((resolve, reject) => {
+      storage[item] ? resolve(storage[item]) : resolve(value);
+    });
+  },
+  setItem: (item, value) => {
+    return new Promise((resolve, reject) => {
+      storage[item] = value;
+      resolve(value);
+    });
+  }
+};
