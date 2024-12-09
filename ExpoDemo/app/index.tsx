1e0f8aea67e6e16c8b5f27f3c8382d0d68bc4afd
Miguel Beltran <m@beltran.work>
2024-12-09 09:44:00 +0100

    Android setup

--- ExpoDemo/app/index.tsx
+++ ExpoDemo/app/index.tsx
@@ -1,6 +1,17 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import RaygunClient, { LogLevel, RaygunClientOptions } from "raygun4reactnative";
 
 export default function Index() {
+
+  const options: RaygunClientOptions = {
+    apiKey: "INSERT_YOUR_API_KEY_HERE",
+    version: "0.1.2",
+    enableCrashReporting: true,
+    logLevel: LogLevel.verbose,
+  };
+  
+  RaygunClient.init(options);
+
   return (
     <View
       style={{
@@ -9,7 +20,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Raygun Demo</Text>
+      <Button title="Send Error" onPress={() => RaygunClient.sendError(Error("Error from Expo app"))} />
     </View>
   );
 }
