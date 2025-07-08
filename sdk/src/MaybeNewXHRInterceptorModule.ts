6d1a23891eaa8687e1f5c0d89178764d0e48c5bd
Miguel Beltran <m@beltran.work>
2025-07-08 08:41:40 +0200

    fixing eslint

--- sdk/src/MaybeNewXHRInterceptorModule.ts
+++ sdk/src/MaybeNewXHRInterceptorModule.ts
@@ -1,5 +1,7 @@
 let XHRInterceptorModule: any = null;
 try {
   XHRInterceptorModule = require('react-native/src/private/inspector/XHRInterceptor');
-} catch {}
+} catch {
+  // Ignore error - module not available in this RN version
+}
 export default XHRInterceptorModule;
